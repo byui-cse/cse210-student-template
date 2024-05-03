df4b933fe53303f309687d2af3a721bd5053f5fb
adamgsheridan <adamgsheridan7@gmail.com>
2024-05-03 16:54:11 -0600

    .

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        int guess = -1;
+        while(guess != magicNumber )
+        {
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        }
     }
 }
